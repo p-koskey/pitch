4139bf64c719e68c0ab734b6c5641d72b9cb4d11
p-koskey <patiencekoskey@gmail.com>
2020-09-21 17:40:32 +0300

    created user and pitch tables

---
+++ app/models.py
@@ -0,0 +1,43 @@
+git from . import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    pass_secure = db.Column(db.String(255))
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    users = db.relationship('User',backref = 'pitch',lazy="dynamic")
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
